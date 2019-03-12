ee48915f903a1f68abceab6f4e9ce2b9f0095fa4
Kalle Bornemark <kalle.bornemark@hiq.se>
2019-03-12 17:59:27 +0100

    feat: extend useFetch to allow post

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,12 +1,19 @@
 import { useState, useEffect } from 'react'
 
-export default function useFetch(url) {
+export default function useFetch(url, method = 'GET', body = null) {
   const [loading, setLoading] = useState(true)
   const [data, setData] = useState(null)
 
   const fetchUrl = async () => {
     try {
-      const response = await fetch(url)
+      const options = {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        ...(body && { body: JSON.stringify(body) }),
+      }
+      const response = await fetch(url, options)
       const json = await response.json()
 
       setData(json)
