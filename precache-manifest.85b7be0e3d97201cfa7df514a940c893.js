4dd83741656d15f73a7809b285458ee83c14ab59
Kalle Bornemark <kalle.bornemark@gmail.com>
2019-04-30 23:41:06 +0200

    Updates

---
+++ precache-manifest.85b7be0e3d97201cfa7df514a940c893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8869c03a467317fe36fd5c79145fde41",
+    "url": "/static/media/logo.8869c03a.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "55f0107825bad4b30940",
+    "url": "/static/js/main.46f73874.chunk.js"
+  },
+  {
+    "revision": "8847acdeec5b94d74f7c",
+    "url": "/static/js/2.a4683f5b.chunk.js"
+  },
+  {
+    "revision": "55f0107825bad4b30940",
+    "url": "/static/css/main.f65c9fb9.chunk.css"
+  },
+  {
+    "revision": "d934ce825ab414a5be8d48676c9b467f",
+    "url": "/index.html"
+  }
+];
