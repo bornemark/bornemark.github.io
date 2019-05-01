f74937fc4c6b1a291483d631443bb2ade77a975e
Kalle Bornemark <kalle.bornemark@gmail.com>
2019-05-01 13:48:26 +0200

    chore: use HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import './styles/index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <App />
   </Router>,
   document.getElementById('root'),
