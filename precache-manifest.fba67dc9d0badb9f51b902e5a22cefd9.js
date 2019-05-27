48a50e3cfe5713f3b8add4c0caded6ca80607e77
Kalle Bornemark <kalle.bornemark@gmail.com>
2019-05-27 22:24:58 +0200

    Updates

--- precache-manifest.fba67dc9d0badb9f51b902e5a22cefd9.js
+++ precache-manifest.fba67dc9d0badb9f51b902e5a22cefd9.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "d1ba0751cee21a02fb9b",
-    "url": "/static/js/main.e153aa0a.chunk.js"
+    "revision": "ce5869ac34b5195971fb",
+    "url": "/static/js/main.0b2d46b9.chunk.js"
   },
   {
-    "revision": "249f135c1a80fb61634e",
-    "url": "/static/js/2.be271120.chunk.js"
+    "revision": "eda0583f72e1a5e82cae",
+    "url": "/static/js/2.5220c06d.chunk.js"
   },
   {
-    "revision": "d1ba0751cee21a02fb9b",
+    "revision": "ce5869ac34b5195971fb",
     "url": "/static/css/main.3cd971c6.chunk.css"
   },
   {
-    "revision": "18ea7058c69575e6e6acb2adbffc26fd",
+    "revision": "96eb6c6c5ea41d3b5f7abe76a919ca0d",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.75a4522a874b4588c58383a1fd6742e3.js"
+  "/precache-manifest.fba67dc9d0badb9f51b902e5a22cefd9.js"
 );
 
 workbox.clientsClaim();
